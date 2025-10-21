7bbb20966b670661e0b7d30c0950076f3eb97a4b
Alex Moon <formsatz@gmail.com>
2025-10-21 17:38:11 +0100

    JO-2 - latest datastar

--- src/component/app/app.ts
+++ src/component/app/app.ts
@@ -1,6 +1,8 @@
 import {Component} from "../component";
 
 export class App extends Component {
-
+    public signals = {
+        example: 'lol',
+    };
 }
 window.customElements.define('app-app', App);
